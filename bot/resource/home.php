25716545c6a7fde56555059bb9a3ffeecda28265
ropsnap <ropsnap9@gmail.com>
2023-05-04 11:39:18 -0300

    Up

--- bot/resource/home.php
+++ bot/resource/home.php
@@ -1817,7 +1817,7 @@ function ccun($message){
 
 	$confibot = $GLOBALS['confibot'];
 
-	bot("editMessageText",array( "message_id" => $message['message_id'] , "chat_id"=> $chat_id , "text" => "💳 Comprar CC Unitária.\n\n⚠️ Alertas\n\n- Todas infos são testadas antes do envio\n- Todas info já vem com nome e cpf","reply_markup" =>$menu,"reply_to_message_id"=> $message['message_id'],"parse_mode" => 'Markdown'));
+	bot("editMessageText",array( "message_id" => $message['message_id'] , "chat_id"=> $chat_id , "text" => "💳 Comprar CC Unitária.\n\n⚠️ Alertas\n\n→ As infos são todas CHK Live 95%\n→ Todas info já vem com Nome e CPF","reply_markup" =>$menu,"reply_to_message_id"=> $message['message_id'],"parse_mode" => 'Markdown'));
 	
 }
 
