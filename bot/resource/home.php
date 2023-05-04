f3d220e917ebd35b10652555d5c37fb42202961a
ropsnap <ropsnap9@gmail.com>
2023-05-04 12:31:36 -0300

    Up

--- bot/resource/home.php
+++ bot/resource/home.php
@@ -1872,6 +1872,6 @@ function ccmix($message){
 	$menu['inline_keyboard'] = $add;
 	$menu['inline_keyboard'][] = [['text'=>"← Voltar",'callback_data'=>"volta_loja"]];
 
-	bot("editMessageText",array( "message_id" => $message['message_id'] , "chat_id"=> $chat_id , "text" => "Está area é resevada para os pacotes mix. Caso não tenha o mix que você procura, entre em contato com o vendedor: *{$confibot['userDono']}* \n $tabela \n","reply_markup" =>$menu,"reply_to_message_id"=> $message['message_id'],"parse_mode" => 'Markdown'));
+	bot("editMessageText",array( "message_id" => $message['message_id'] , "chat_id"=> $chat_id , "text" => "Está area é resevada para os pacotes mix. Caso não tenha o mix que você procura, contate o vendedor *{$confibot['userDono']}* \n $tabela \n","reply_markup" =>$menu,"reply_to_message_id"=> $message['message_id'],"parse_mode" => 'Markdown'));
 	
 }
